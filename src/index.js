bc864c30f309cdb9e2796245357f74d683e4ac40
emily <mle4d@me.com>
2019-09-06 10:57:19 -0700

    env and lint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import Auth0Provider from './Auth0Provider';
 
 render(
-  <App />,
+  <Auth0Provider 
+    domain={process.env.AUTH0_DOMAIN}
+    client_id={process.env.AUTH0_CLIENT_ID}
+    redirect_uri={process.env.AUTH0_CALLBACK}>
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
 );
